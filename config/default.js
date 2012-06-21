e30ba24e3bc72b12f2b5262a114b20772ebd0c58
Makis Tracend <makis.tracend@gmail.com>
2012-06-21 03:05:21 -0700

    #14 Dynamically updating paths in client.js based on config

--- config/default.js
+++ config/default.js
@@ -1,6 +1,17 @@
 module.exports = config = {
-  "name" : "CRUDr",
-  "static" : {
-	 "client.js": "/crudr/client.js"
-  }
+	"name" : "CRUDr",
+	"host" : "localhost", 
+	"static" : {
+		"client.js": "/crudr/client.js", 
+		"socket.io.js": "/socket.io/socket.io.js"
+	}, 
+	"oauth" : {
+		"authorize" : "/oauth/authorize"
+	}, 
+	"routes" : {
+		"create" : "/crudr/create", 
+		"read" : "/crudr/read", 
+		"update" : "/crudr/update", 
+		"delete" : "/crudr/delete"
+	}
 }
