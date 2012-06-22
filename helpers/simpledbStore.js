c1c050abc4f20c16bc7921b6af74027ff9edff55
Makis Tracend <makis.tracend@gmail.com>
2012-06-22 14:28:18 -0700

    #13 Normalizing behaviour for read / delete states

--- helpers/simpledbStore.js
+++ helpers/simpledbStore.js
@@ -16,7 +16,10 @@
 module.exports = function(aws) {
     return function(req, res, next) {
 		
-		// create the domain if not availabe
+		aws.sdb.listDomains({}, 'sdb.amazonaws.com').onSuccess(function() {
+		  console.log(this.requestId, this.data);
+		})
+		// create the domain (if not availabe)
 		aws.sdb.createDomain({ domainName: req.backend});
 		
         var callback = function(err, result) {
@@ -31,6 +34,69 @@ module.exports = function(aws) {
 					if( key == "id" ) continue;
 					attributes.push({ name: key, value: req.model[key] });
                 }
+				
+                if( typeof( req.model.id ) == "undefined" ) req.model.id = (new Date).getTime();
+				
+				aws.sdb.putAttributes(
+				  {
+					domainName: req.backend,
+					itemName: req.model.id,
+					attributes: attributes,
+				  }
+				  // You can optionally override the default endpoint on a
+				  // per-request basis as well.
+				  //'sdb.us-west-1.amazonaws.com'
+				).onSuccess(function() {
+				  //console.log(this.requestId, this.data);
+				  res.end(req.model);
+				}).onFailure(function() {
+				  console.log(this.requestId, this.error);
+				  if (this.error) return next(this.error);
+                    res.end(req.model);
+				});
+            },
+            
+            read: function() {
+				
+				var query = "select * from "+ req.backend;
+				
+				if (req.model.id) {
+					query += " where  itemName() = '"+ req.model.id +"'";
+				}
+				
+                aws.sdb.select({ selectExpression: query })
+					
+					.onSuccess(function() {
+					  // it worked!
+					  //console.log(this.requestId, this.data);
+					  // deconstruct the response to an array
+					  var collection = [];
+					  for( i in this.data.items ){
+						  var model = {};
+						  model.id  = this.data.items[i].name;
+						  var attributes = this.data.items[i].attributes;
+						  for( k in attributes ){
+							  model[attributes[k].name] = attributes[k].value;
+						  }
+						  collection.push(model);
+					  }
+					  //console.log( collection );
+					  res.end(collection);
+					  
+					}).onFailure(function() {
+					  console.log(this.requestId, this.error);
+					  if (this.error) return next(this.error);
+						res.end(req.model);
+					});
+
+            },
+            
+            update: function() {
+                var attributes = [];
+                for (var key in req.model) {
+					if( key == "id" ) continue;
+					attributes.push({ name: key, value: req.model[key], replace: true });
+                }
                 var id = ( typeof( req.model.id ) != "undefined" ) ? req.model.id : (new Date).getTime();
 				
 				aws.sdb.putAttributes(
@@ -44,57 +110,8 @@ module.exports = function(aws) {
 				  //'sdb.us-west-1.amazonaws.com'
 				).onSuccess(function() {
 				  // it worked!
-				  console.log(this.requestId, this.data);
-				  callback(this.error, req.model);
-				}).onFailure(function() {
-				  // uh oh!
-				  console.log(this.requestId, this.error);
-				  if (this.error) return next(this.error);
-                    res.end(req.model);
-				});
-            },
-            
-            read: function() {
-                if (req.model.id) {
-                    aws.sdb.select({ selectExpression: "select * from "+ req.backend +" where  itemName() = '"+ req.model.id +"'" })
-					
-					.onSuccess(function() {
-					  // it worked!
-					  console.log(this.requestId, this.data);
-					  callback(this.error, req.model);
-					}).onFailure(function() {
-					  // uh oh!
-					  console.log(this.requestId, this.error);
-					  if (this.error) return next(this.error);
-						res.end(req.model);
-					});
-
-                } else {
-                    return false;
-                }
-            },
-            
-            update: function() {
-                var attributes = [];
-                for (var key in req.model) {
-					if( key == "id" ) continue;
-					attributes.push({ name: key, value: req.model[key], replace: true });
-                }
-                var id = ( typeof( req.model.id ) != "undefined" ) ? req.model.id : (new Date).getTime();
-				
-				console.log( attributes );
-                aws.sdb.putAttributes(
-				  {
-					domainName: req.backend,
-					itemName: id,
-					attributes: attributes,
-				  }
-				  // You can optionally override the default endpoint on a
-				  // per-request basis as well.
-				  //'sdb.us-west-1.amazonaws.com'
-				).onSuccess(function() {
-				  // it worked!
-				  console.log(this.requestId, this.data);
+				  //console.log(this.requestId, this.data);
+				  res.end(req.model);
 				}).onFailure(function() {
 				  // uh oh!
 				  console.log(this.requestId, this.error);
@@ -118,8 +135,8 @@ module.exports = function(aws) {
 					attributes: attributes,
 					expected: [ { name: "quantity", value: "1", exists: true } ]
 				}).onSuccess(function() {
-				  // it worked!
-				  console.log(this.requestId, this.data);
+				  //console.log(this.requestId, this.data);
+				  res.end(req.model);
 				}).onFailure(function() {
 				  // uh oh!
 				  console.log(this.requestId, this.error);
