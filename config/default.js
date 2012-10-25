aa86e2df880907d0185183f4baf6e2684a47326e
Makis Tracend <makis.tracend@gmail.com>
2012-10-25 16:28:03 -0700

    #23 Removing static/oauth enties in default config (in favor of a unified routes obj)

--- config/default.js
+++ config/default.js
@@ -1,13 +1,6 @@
 module.exports = config = {
 	"name" : "crudr",
 	"host" : "localhost", 
-	"static" : {
-		"client.js": "/crudr/client.js", 
-		"socket.io.js": "/socket.io/socket.io.js"
-	}, 
-	"oauth" : {
-		"authorize" : "/oauth/authorize"
-	}, 
 	"domain_prefix" : "", 
 	"routes" : {
 		"oauth" : "/oauth", 
