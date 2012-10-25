fd1fb53dd74638a74e916d298979f6fb01a3b64a
Makis Tracend <makis.tracend@gmail.com>
2012-10-25 16:38:26 -0700

    Adding another (scaffolding) test

---
+++ test/crudr.test.js
@@ -0,0 +1,13 @@
+
+
+describe('CRUDr', function () {
+
+  describe('listen', function () {
+    it ('should initiate the sockets. create the backends and return the io object', function (done) {
+      done()
+    })
+  })
+
+})
+
+
