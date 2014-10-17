6af4b9582d62f45b50b6347b72e1e2c0a06adceb
Makis Tracend <makis.tracend@gmail.com>
2014-10-16 18:15:48 -0700

    disabling unused config

--- config/default.js
+++ config/default.js
@@ -1,19 +1,21 @@
 module.exports = config = {
-	"name" : "crudr",
-	"host" : "localhost",
-	"domain_prefix" : "",
-	"routes" : {
-		"oauth" : "/oauth",
-		"auth" : "auth",
+	name: "crudr",
+	host: "localhost",
+	//domain_prefix: "",
+	routes: {
+	//	"oauth" : "/oauth",
+	//	"auth" : "auth",
 		"rest" : "/data",
 		"client": "/crudr.js"
 	},
+	/*
 	"action" : {
 		"register" : "/crudr/register",
 		"reset" : "/crudr/reset"
 	},
+	*/
 	backends: [], // an array or object of every backend binded
 	store: "memory", // the type of store to be used, if a common store type is applicable
-	"scope" : {},
-	"namespace" : false
+	scope: [], // array of attributes to filter the data
+	namespace: false
 }
