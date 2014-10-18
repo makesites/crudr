6f3d5c50fafe00ddf471dc959f150465796b1590
Makis Tracend <makis.tracend@gmail.com>
2014-10-18 03:14:36 -0700

    separating backend config from crudr options

--- config/default.js
+++ config/default.js
@@ -1,22 +1,10 @@
+// this is sample config of a list of backends
 module.exports = config = {
-	name: "crudr",
-	host: "localhost",
-	auth: false,
-	//domain_prefix: "",
-	routes: {
-	//	"oauth" : "/oauth",
-	//	"auth" : "auth",
-		"rest" : "/data",
-		"client": "/crudr.js"
-	},
-	/*
-	"action" : {
-		"register" : "/crudr/register",
-		"reset" : "/crudr/reset"
-	},
-	*/
+	hosts: [], // limit requests from specific hosts
 	backends: [], // an array or object of every backend binded
 	store: "memory", // the type of store to be used, if a common store type is applicable
-	scope: [], // array of attributes to filter the data
-	namespace: false
+	scope: [], // array of attributes, used to filter the data transmitted
+	namespace: false, // limit backends to sockets of a specific namespace
+	db: [], // list of database collections, matching the backend definitions
+	sync: [] // remote methods (middleware) to be triggered on sync
 }
